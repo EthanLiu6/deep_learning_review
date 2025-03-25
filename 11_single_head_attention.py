66b69bfcaf7701ec0d7b40cee63e3636ef9ba5ec
EthanLiu6 <liue2336@gmail.com>
2025-03-26 00:13:03 +0800

    update: code 11

--- 11_single_head_attention.py
+++ 11_single_head_attention.py
@@ -8,20 +8,20 @@ import torch.nn.functional as F
 
 
 class SingleHeadAttention(nn.Module):
-    def __init__(self, ipt: Tensor, *args):  # please insure your input shape like:
-        # [batch_size, seq_len, embedding_dim]
+    def __init__(self, embedding_dim, *args):
+
         super(SingleHeadAttention, self).__init__()
-        self.embedding_dim = ipt.shape[-1]
+        self.embedding_dim = embedding_dim
         self.qkv = nn.Linear(self.embedding_dim, self.embedding_dim * 3)
 
-    def forward(self, X):
-
+    def forward(self, X):  # please insure your input shape like:
+        # [batch_size, seq_len, embedding_dim]
         # 1. q,k,v   2.q @ k^t   3.mask(ignore)   4.softmax scale   5.attention res
         qkv: Tensor = self.qkv(X)
-        query, key, value = torch.split(qkv, [self.embedding_dim, self.embedding_dim, self.embedding_dim])
-        print(query.shape)
-        print(key.shape)
-        print(value.shape)
+        query, key, value = torch.split(qkv, [self.embedding_dim, self.embedding_dim, self.embedding_dim], dim=-1)
+        # print(query.shape)
+        # print(key.shape)
+        # print(value.shape)
 
         attention_scores = torch.matmul(query, key.transpose(-2, -1))
         scores = F.softmax(attention_scores, dim=-1)
@@ -36,6 +36,12 @@ class SingleHeadAttention(nn.Module):
         return attention_res, attention_scores
 
 
+if __name__ == '__main__':
+    ipt = torch.randn(2, 3, 64)
+    model = SingleHeadAttention(ipt.shape[-1])
+    attention, scores = model(ipt)
 
-
+    print(model)
+    print(attention.shape)
+    print(scores.shape)
 
